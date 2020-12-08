b749f5841cd0ffe5f4afcce47a4bf97c1e25f3d9
amr33mohamd <amr2010mohamd2010@gmail.com>
2020-12-08 23:42:22 +0200

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,375 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <!-- basic -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <!-- mobile metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+      <!-- site metas -->
+      <title>Savely</title>
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <!-- bootstrap css -->
+      <link rel="stylesheet" href="/css/bootstrap.min.css">
+      <!-- style css -->
+      <link rel="stylesheet" href="/css/style.css">
+      <!-- Responsive-->
+      <link rel="stylesheet" href="/css/responsive.css">
+      <!-- fevicon -->
+      <link rel="icon" href="/images/fevicon.png" type="image/gif" />
+      <!-- Scrollbar Custom CSS -->
+      <link rel="stylesheet" href="/css/jquery.mCustomScrollbar.min.css">
+      <!-- Tweaks for older IEs-->
+      <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
+      <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+   </head>
+   <!-- body -->
+   <body class="main-layout">
+      <!-- loader  -->
+      <div class="loader_bg">
+         <div class="loader"><img src="images/loading.gif" alt="#" /></div>
+      </div>
+      <!-- end loader -->
+      <!-- header -->
+      <header>
+         <!-- header inner -->
+         <div class="header">
+         <div class="container">
+            <div class="row">
+               <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
+                  <div class="full">
+                     <div class="center-desk">
+                        <div class="logo"> <a href="index.html"><img src="/images/logo.png" style="height:50px"/></a> </div>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
+                  <div class="menu-area">
+                     <div class="limit-box">
+                        <nav class="main-menu">
+                           <ul class="menu-area-main">
+                              <li class="active"> <a href="index.html">Home</a> </li>
+                              <li> <a href="#about">About</a> </li>
+                              <li> <a href="#service"> Service</a> </li>
+                              <li> <a href="#testimonial">Contact us</a> </li>
+                           </ul>
+                        </nav>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <!-- end header inner -->
+      </header>
+      <!-- end header -->
+      <section class="slider_section">
+         <div id="myCarousel" class="carousel slide banner-main" data-ride="carousel">
+            <div class="carousel-inner">
+               <div class="carousel-item active">
+                  <img class="first-slide" src="images/banner2.png"  style="height:750px;width:100%;    filter: brightness(50%);" alt="First slide">
+                  <div class="container">
+                     <div class="carousel-caption relative">
+                        <h1>Savely Services</h1>
+                        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and </p>
+                        <a  href="#">Read More</a>
+                     </div>
+                  </div>
+               </div>
+               <div class="carousel-item">
+                  <img class="second-slide" src="https://images.unsplash.com/photo-1486406146926-c627a92ad1ab?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2100&q=80" style="height:750px;width:100%;    filter: brightness(50%);" alt="Second slide">
+                  <div class="container">
+                     <div class="carousel-caption relative">
+                        <h1>Basic template</h1>
+                        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and </p>
+                        <a  href="#">Read More</a>
+                     </div>
+                  </div>
+               </div>
+               <div class="carousel-item">
+                  <img class="third-slide" src="https://images.unsplash.com/photo-1482990673175-a0cb26bf62bc?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2345&q=80" style="height:750px;width:100%;    filter: brightness(50%);" alt="Third slide">
+                  <div class="container">
+                     <div class="carousel-caption relative">
+                        <h1>Basic template</h1>
+                        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and </p>
+                        <a  href="#">Read More</a>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+            <i class='fa fa-angle-left'></i>
+            </a>
+            <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+            <i class='fa fa-angle-right'></i>
+            </a>
+         </div>
+      </section>
+      <!-- about  -->
+      <div id="about" class="about top_layer">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12">
+                  <div class="titlepage">
+                     <h2>About Informations</h2>
+                     <span>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and </span>
+                  </div>
+               </div>
+            </div>
+            <div class="row">
+               <div class="col-md-12">
+                  <div class="img-box">
+                     <figure><img src="images/about.png" alt="img"/></figure>
+                     <a href="#">read more</a>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- end abouts -->
+      <!-- service -->
+      <div id="service" class="service">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12">
+                  <div class="titlepage">
+                     <h2>Services </h2>
+                     <span>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of <br>using Lorem Ipsum is that it has a more-or-less normal distribution of letters,</span>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="container margin-r-l">
+            <div class="row">
+               <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 thumb">
+                  <div class="service-box">
+                     <figure>
+                        <a href="images/1.jpg" class="fancybox" rel="ligthbox">
+                        <img style="    filter: brightness(50%);" src="images/1.jpg" class="zoom img-fluid "  alt="">
+                        </a>
+                        <span class="hoverle">
+                        <a href="images/1.jpg" class="fancybox" rel="ligthbox">Create Saber Accounts for your Business
+</a>
+                        </span>
+                     </figure>
+                  </div>
+               </div>
+               <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 thumb">
+                  <div class="service-box">
+                     <figure>
+                        <a href="images/2.jpg" class="fancybox" rel="ligthbox">
+                        <img style="    filter: brightness(50%);" src="images/2.jpg" class="zoom img-fluid "  alt="">
+                        </a>
+                        <span class="hoverle">
+                        <a href="images/1.jpg" class="fancybox" rel="ligthbox">Adding Products and certificate extracting
+
+</a>
+                        </span>
+                     </figure>
+                  </div>
+               </div>
+               <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 thumb">
+                  <div class="service-box">
+                     <figure>
+                        <a href="images/3.jpg" class="fancybox" rel="ligthbox">
+                        <img style="    filter: brightness(50%);" src="images/3.jpg" class="zoom img-fluid "  alt="">
+                        </a>
+                        <span class="hoverle">
+                        <a href="images/3.jpg" class="fancybox" rel="ligthbox">Extract the Certificate of conformity
+
+</a>
+                        </span>
+                     </figure>
+                  </div>
+               </div>
+               <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 thumb">
+                  <div class="service-box">
+                     <figure>
+                        <a href="images/4.jpg" class="fancybox" rel="ligthbox">
+                        <img style="    filter: brightness(50%);"  src="images/4.jpg" class="zoom img-fluid "  alt="">
+                        </a>
+                        <span class="hoverle">
+                        <a href="images/4.jpg" class="fancybox" rel="ligthbox">Requests Track and Following up
+
+</a>
+                        </span>
+                     </figure>
+                  </div>
+               </div>
+               <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 thumb">
+                  <div class="service-box">
+                     <figure>
+                        <a href="images/5.jpg" style="    filter: brightness(50%);" class="fancybox" rel="ligthbox">
+                        <img  src="images/5.jpg"  class="zoom img-fluid "  alt="">
+                        </a>
+                        <span class="hoverle">
+                        <a href="images/5.jpg" class="fancybox" rel="ligthbox" >Create Exemption requests
+
+</a>
+                        </span>
+                     </figure>
+                  </div>
+               </div>
+
+            </div>
+         </div>
+      </div>
+      <!-- end service -->
+      <!-- Download -->
+
+      <!-- end Download -->
+
+
+      <div id="testimonial" class="testimonial" style="background-color:#fff">
+         <div class="container">
+
+            <div class="row d-flex justify-content-center">
+
+               <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 d-flex justify-content-center align-center">
+                  <div class="contact">
+                     <h3>Request Service</h3>
+                     <form>
+                        <div class="row">
+                           <div class="col-sm-12">
+                              <input class="contactus" placeholder="Name" type="text" name="name">
+                           </div>
+                           <div class="col-sm-12">
+                              <input class="contactus" placeholder="Phone" type="text" name="phone">
+                           </div>
+                           <div class="col-sm-12">
+                              <input class="contactus" placeholder="Email" type="text" name="email">
+                           </div>
+                           <div class="col-sm-12">
+                             <input class="contactus" placeholder="Company" type="Company" name="company">
+                           </div>
+                           <div class="col-sm-12">
+                             <input class="contactus" placeholder="Company" type="file" name="company">
+                           </div>
+                           <div class="col-sm-12">
+                              <button class="send">Send</button>
+                           </div>
+                        </div>
+                     </form>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+
+
+      <!-- Testimonial -->
+      <div id="testimonial" class="testimonial">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12">
+                  <div class="titlepage">
+                     <h3>Testimonial</h3>
+                  </div>
+               </div>
+            </div>
+            <div class="row d-flex justify-content-center">
+
+               <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 d-flex justify-content-center align-center">
+                  <div class="contact">
+                     <h3>Contact Us</h3>
+                     <form>
+                        <div class="row">
+
+                           <div class="col-sm-12">
+                              <input class="contactus" placeholder="Phone" type="text" name="Email">
+                           </div>
+
+                           <div class="col-sm-12">
+                              <textarea class="textarea" placeholder="Message" type="text" name="Message"></textarea>
+                           </div>
+                           <div class="col-sm-12">
+                              <button class="send">Send</button>
+                           </div>
+                        </div>
+                     </form>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- end Testimonial -->
+      <!--  footer -->
+      <footr>
+         <div class="footer">
+            <div class="container">
+               <div class="row">
+                  <div class="col-lg-4 col-md-6 col-sm-12 width">
+                     <div class="address">
+                        <h3>Address</h3>
+                        <i><img src="icon/3.png">Locations</i>
+                     </div>
+                  </div>
+                  <div class="col-lg-4 col-md-6 col-sm-12 width">
+                     <div class="address">
+                        <h3>Menus</h3>
+                        <h7 style="color:#fff">Home</h7>
+                        <br>
+                        <h7 style="color:#fff">About</h7>
+                        <br>
+                        <h7 style="color:#fff">Services</h7>
+                        <br>
+                        <h7 style="color:#fff">Contact us</h7>
+
+
+                     </div>
+                  </div>
+
+                  <div class="col-lg-4 col-md-6 col-sm-12 width">
+                     <div class="address">
+                        <h3>Social Media </h3>
+                        <ul class="contant_icon">
+                           <li><img src="icon/fb.png" alt="icon"/></li>
+                           <li><img src="icon/tw.png" alt="icon"/></li>
+                           <li><img src="icon/lin (2).png" alt="icon"/></li>
+                           <li><img src="icon/instagram.png" alt="icon"/></li>
+                        </ul>
+                     </div>
+                  </div>
+
+               </div>
+            </div>
+            <div class="copyright">
+               <p>Copyright 2020 All Right Reserved By Savely</p>
+            </div>
+         </div>
+      </footr>
+      <!-- end footer -->
+      <!-- Javascript files-->
+      <script src="/js/jquery.min.js"></script>
+      <script src="/js/popper.min.js"></script>
+      <script src="/js/bootstrap.bundle.min.js"></script>
+      <script src="/js/jquery-3.0.0.min.js"></script>
+      <script src="/js/plugin.js"></script>
+      <!-- sidebar -->
+      <script src="/js/jquery.mCustomScrollbar.concat.min.js"></script>
+      <script src="/js/custom.js"></script>
+      <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
+      <script>
+         $(document).ready(function(){
+         $(".fancybox").fancybox({
+         openEffect: "none",
+         closeEffect: "none"
+         });
+
+         $(".zoom").hover(function(){
+
+         $(this).addClass('transition');
+         }, function(){
+
+         $(this).removeClass('transition');
+         });
+         });
+
+      </script>
+   </body>
+</html>
